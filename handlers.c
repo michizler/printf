ef7d2d715f8557ecd11c911bab92abd7a79e95b0
Bright Uzosike <brightuzosike@gmail.com>
2022-07-13 12:57:42 -0700

    Betty style modification for handlers.c

--- handlers.c
+++ handlers.c
@@ -160,7 +160,8 @@ int handle_precision(va_list args, const char *modifier, char *index)
 /**
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
- * @specifier: A pointer to a potential conversion specifier.
+ * @int: An expected integer
+ * @char: Expected character.
  *
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
