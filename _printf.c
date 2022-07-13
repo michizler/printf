274d93777d9af1dccc7a5ccd05c6e52d100549b9
Bright Uzosike <brightuzosike@gmail.com>
2022-07-13 14:09:08 -0700

    Betty doc style modifications for _printf.c

--- _printf.c
+++ _printf.c
@@ -29,6 +29,14 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	int i, wid, prec, ret = 0;
 	char tmp;
 	unsigned char flags, len;
+
+/**
+ * int - function pointer.
+ * @f: function pointer variable with expected
+ * args.
+ * Return: An unsigned value of buffer
+ */
+
 	unsigned int (*f)(va_list, buffer_t *,
 			unsigned char, int, int, unsigned char);
 
